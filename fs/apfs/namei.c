b01f894fd23405f6ba067bb1f7fa37050cbf67ec
Ernesto A. Fernández <ernesto.mnd.fernandez@gmail.com>
2018-03-24 18:51:16 -0300

    apfs: stop printing the cnid on every lookup
    When the directory lookup code was first added, we were mostly going
    blind into the filesystem and it seemed like this could help with
    testing. It has become very annoying, so get rid of it.
    
    Signed-off-by: Ernesto A. Fernández <ernesto.mnd.fernandez@gmail.com>

--- fs/apfs/namei.c
+++ fs/apfs/namei.c
@@ -18,11 +18,8 @@ static struct dentry *apfs_lookup(struct inode *dir, struct dentry *dentry,
 		return ERR_PTR(-ENAMETOOLONG);
 
 	ino = apfs_inode_by_name(dir, &dentry->d_name);
-	if (ino) {
+	if (ino)
 		inode = apfs_iget(dir->i_sb, ino);
-		/* The module can't do much yet, so test by printing the cnid */
-		apfs_msg(dir->i_sb, KERN_INFO, "inode found: %llu", ino);
-	}
 	return d_splice_alias(inode, dentry);
 }
 
