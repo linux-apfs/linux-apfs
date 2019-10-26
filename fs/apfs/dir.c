b76895c31e38388f56549c13a5d2a60019794e13
Ernesto A. Fernández <ernesto.mnd.fernandez@gmail.com>
2019-10-26 00:18:21 -0300

    apfs: get rid of redundant timespec functions
    Get rid of apfs_timestamp() and apfs_timespec(), and use instead the
    common timespec64_to_ns() and ns_to_timespec64().
    
    Signed-off-by: Ernesto A. Fernández <ernesto.mnd.fernandez@gmail.com>

--- fs/apfs/dir.c
+++ fs/apfs/dir.c
@@ -276,6 +276,7 @@ static int apfs_build_dentry_val(struct inode *inode, u64 sibling_id,
 	struct apfs_x_field xkey;
 	int total_xlen = 0, val_len;
 	__le64 raw_sibling_id = cpu_to_le64(sibling_id);
+	struct timespec64 now = current_time(inode);
 
 	/* The dentry record may have one xfield: the sibling id */
 	if (sibling_id)
@@ -289,7 +290,7 @@ static int apfs_build_dentry_val(struct inode *inode, u64 sibling_id,
 	*val_p = val;
 
 	val->file_id = cpu_to_le64(apfs_ino(inode));
-	val->date_added = apfs_timestamp(current_time(inode));
+	val->date_added = cpu_to_le64(timespec64_to_ns(&now));
 	val->flags = cpu_to_le16((inode->i_mode >> 12) & 15); /* File type */
 
 	if (!sibling_id)
